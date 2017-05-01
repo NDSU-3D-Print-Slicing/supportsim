f1c004ae22382ec4baa5f1e1c73c694c3d9389dd
Lou <DangerousElectrician@safeelectrons.net>
2017-05-01 17:02:23 -0500

    now it draws the support structure properly

--- support.py
+++ support.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
+from math import sqrt
 
 class SupportSim():
 
 	def __init__(self):
-		print("construct")
+		#print("construct")
 		self.pointx = np.array([]).reshape(0,2)
 		self.pointy = np.array([]).reshape(0,2)
 		self.nodes =  np.array([]).reshape(0,2)
 		self.beams= []
 
 
-	def drawlines(self):
+	def drawstructure(self):
 		for n1,n2 in self.beams:
-			plt.plot(self.nodes[n1], self.nodes[n2])
+			p = np.array([self.nodes[n1], self.nodes[n2]]).T
+			plt.plot(p[0], p[1])
 			print("plotting ", self.nodes[n1],";", self.nodes[n2])
 
 		plt.show()
+	
+	def beamlength(self):
+		length = 0
+		for n1,n2 in self.beams:
+			#length = sqrt(self.nodes[n1])
+			print(self.nodes[n1], self.nodes[n2])
+			length += np.linalg.norm(self.nodes[n1]- self.nodes[n2])
+		return length
 
 	def addnode(self, x, y):
 		self.nodes = np.append(self.nodes, [[x, y]], axis=0)
--- test.py
+++ test.py
@@ -5,6 +5,7 @@ a = support.SupportSim()
 
 def diagonalsplit(size):
 	supportpoints = np.zeros((size[0], 2))
+	a.addnode(5,0)
 	for i in range(len(supportpoints)):
 		supportpoints[i] = [i,size[1]]
 		a.addnode(i, size[1])
@@ -16,6 +17,10 @@ def diagonalsplit(size):
 	print(supportpoints)
 	for n in range(len(a.nodes)):
 		a.addbeam(n, 0)
+	#a.addnode(1,3)
+	#a.addnode(10,8)
+	#a.addbeam(0,1)
 
 diagonalsplit((10,10))
-a.drawlines()
+print(a.beamlength())
+a.drawstructure()
